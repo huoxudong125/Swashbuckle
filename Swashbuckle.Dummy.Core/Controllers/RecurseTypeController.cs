0b6b431f404959873e85571813a1ee33e15df832
huoxudong125 <423huo@163.com>
2016-03-01 11:06:09 +0800

    Add  RecurseTypeController

---
+++ Swashbuckle.Dummy.Core/Controllers/RecurseTypeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Swashbuckle.Dummy.Controllers
+{
+ 
+    public class RecursiveTypeController:ApiController
+    {
+        [HttpGet]
+        public List<MenuItem> GetMenuItems()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class MenuItem
+    {
+
+        public String description;
+        public String title;
+
+        public List<MenuItem> sub;
+
+        public MenuItem(String title, String description)
+        {
+            this.title = title;
+            this.description = description;
+            this.sub = new List<MenuItem>();
+        }
+
+    }
+}
